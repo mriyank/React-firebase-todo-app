ad934c1976dc6e6892c3fea3fe38c449e45ee9f5
mriyank <48956758+mriyank@users.noreply.github.com>
2020-09-11 23:01:05 +0530

    added material design

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -1,21 +1,31 @@
 import React,{useState} from 'react';
+import { Button } from '@material-ui/core';
 import './App.css';
 
 function App() {
-  const [todos, seTodos] = useState(['Take dogs for a walk','Take the rubbish out',"it's me Mriyank!!"]);
+  const [todos, setTodos] = useState(['Take dogs for a walk','Take the rubbish out',"it's me Mriyank!!"]);
   const[input, setInput] = useState('');
   console.log(input);
   const addTodo = (event) => {
     // this will fire off when we click the button
+    event.preventDefault(); //I will stop the refresh
     console.log('*','I m workong');
+    setTodos([...todos, input]);
+    setInput(''); // clear up input after clicking add todo button
   }
 
   return (
     <div className="App">
       <h1>Hello World </h1>
+      <form>
 
       <input value={input} onChange={event => setInput(event.target.value)}/>
-      <button onClick={addTodo}>Add Todo</button>
+      {/* <button type="submit" onClick={addTodo}>Add Todo</button> */}
+      <Button type="submit" onClick={addTodo} variant="contained" color="primary">
+        Add Todo
+      </Button>
+
+      </form>
       <ul>
         {todos.map(todo =>(
           <li>{todo}</li>
